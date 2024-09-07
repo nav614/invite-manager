b73dbf49e1877f70b7efa45e344a93c201c9ad63
Anatoly Novosiletskiy <anatoly@infomentor.co.uk>
2024-09-07 14:57:45 +0200

    added task and readme

---
+++ db_script.sql
@@ -0,0 +1,18 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL, 
+    is_verified BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE invites (
+    id SERIAL PRIMARY KEY,
+    inviter_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    invitee_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    is_accepted BOOLEAN DEFAULT FALSE,
+    is_pending BOOLEAN DEFAULT TRUE,
+    permissions JSONB NOT NULL, 
+    expiration_date TIMESTAMP,  
+    invite_date TIMESTAMP DEFAULT NOW(),
+    accepted_at TIMESTAMP
+);
